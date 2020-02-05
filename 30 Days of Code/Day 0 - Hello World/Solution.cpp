1614fa6a7a95fcb13400884c6c8ea6ae78f23678
Christopher MacIntosh <schecterchris@outlook.com>
2020-02-05 14:38:14 -0800

    Added Day 0 Solutions

---
+++ 30 Days of Code/Day 0 - Hello World/Solution.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    string input_string; 
+    
+    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+    getline(cin, input_string); 
+    
+    // Print a string literal saying "Hello, World." to stdout using cout.
+    cout << "Hello, World." << endl;
+
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    //std::cout is a throwback to the C++ class I took in the late 2000's
+    //you can also just use "cout" as shown above in the "Hello World" portion of the example
+    std::cout << input_string;
+    return 0;
+}
